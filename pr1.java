3d1e791d28be5d5ac38007b9d5e5cb9e3bf5ef33
Navya S N <100507775+Navyasngowda@users.noreply.github.com>
2022-09-26 21:35:04 +0530

    cons-demo prm1

--- pr1.java
+++ pr1.java
@@ -1,51 +1,35 @@
-class CONS_DEMO
-{
-    double l;
-    double b;
-    double h;
-    
-
-CONS_DEMO(Double l,Double b,Double h)
-{
-    this.l=l;
-    this.b=b;
-    this.h=h;
-}
-
-CONS_DEMO(Double l)
-{
-    this.l=b=h=l;
-    
-}
+class cons_box {
+    double l,b,h;
+cons_box(double l,double h,double b)
+    {
+        this.l=l;
+        this.b=b;
+        this.h=h;
+    }
+    cons_box(double l) {
+       l=h=b=l;
+    }
 double volume()
 {
-    return l*b*h;
+   return l*b*h;
 }
-
-void print()
-{
-    System.out.println("method overloaded");
-}
-void print(double v)
-{
-    System.out.printf("VOLUME OF cuboid="+v);
-    
-}
-}
-
-public class CONSTRUCTOR_DEMO {
- public static void main(String[] args) {
-    CONS_DEMO cuboid=new CONS_DEMO(10.0,20.0,30.0);
-    CONS_DEMO cube=new CONS_DEMO(4.0);
-    double v1=cuboid.volume();
-    double v2=cube.volume();
-    cuboid.print();
-    cube.print();
-    cuboid.print(v1);
-    cube.print(v2);
-
-        
+    void print()
+    {
+        System.out.println("method overloading");
     }
-
-}    
+        void print(double v)
+        {
+            System.out.println("volume of box="+v);
+        }
+}
+public class cons_demo {
+    public static void main(String[] args) {
+        cons_box cuboid=new cons_box(10,20,10);
+        cons_box cube=new cons_box(10);
+        double v1=cuboid.volume();
+        double v2=cube.volume();
+        cuboid.print();
+        cube.print(v1);
+        cube.print(v2);
+    }
 
